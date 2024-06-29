6088af2b29d9ad06f47f484d0a32c8a526cd897a
yamakoud <kyamad9@gmail.com>
2024-06-29 17:41:53 +0900

    add validateBody

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -6,6 +6,12 @@ export class Halidator extends Validator {
   constructor(options: ValidatorOptions = {}) {
     super(new BrowserDOM(), options);
   }
+
+  static validateBody(): Promise<any> {
+    const validator = new Halidator();
+    const bodyHTML = document.body.outerHTML;
+    return validator.validate(bodyHTML);
+  }
 }
 
 if (typeof window !== 'undefined') {
